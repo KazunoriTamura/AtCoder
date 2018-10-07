c995223fd7846c76e8d141e51672881b73e34166
KazunoriTamura <tamura.kazunori.78n@st.kyoto-u.ac.jp>
2018-10-08 01:47:36 +0900

    追加

--- AtCoderBeginnersSelection/008_Otoshidama.py
+++ AtCoderBeginnersSelection/008_Otoshidama.py
@@ -1,26 +1,52 @@
 N,Y = input().split()
 # print(N,Y)
+N = int(N)
+Y = int(Y)
 
-a = [int(N),0,0]
+a_old = [int(N),0,0]
 # print(a)
 
 def check(a,Y):
     Z = 10000*a[0] + 5000*a[1] + 1000*a[2]
-    if Y == Z:
+    # print(Z)
+    if Z == Y:
         return 1
     else:
         return 0
 
-flag = 0
-for i in range(N):
-    N_new = N - i
-    a[0] = N_new
-    if check(a,Y):
-        flag = 1
+def compute_part(a_new,Y):
+    a_new_2 = [0,0,0]
+    for i in range(a_new[1]+1):
+        a_new_2[0] = a_new[0]
+        a_new_2[1] = a_new[1] - i
+        a_new_2[2] = a_new[2] + i
+        # print(a_new_2)
+        c = check(a_new_2,Y)
+        # print(c)
+        if c:
+            a_answer = a_new_2
+            flag = 1
+            break
+        else:
+            a_answer = [-1,-1,-1]
+            flag = 0
+    return a_answer,flag
+
+# flag = 0
+a_answer = [-1,-1,-1]
+a_new = [0,0,0]
+for i in range(N+1):
+    a_new[0] = a_old[0] - i
+    a_new[1] = a_old[1] + i
+    a_new[2] = a_old[2]
+    a_answer,flag = compute_part(a_new,Y)
+    if flag:
         break
 
-if flag:
-    return a
+# print(flag)
+# print(a_answer)
 
-flag2 = 0
-for i in range()
+if flag:
+    print(a_answer[0],a_answer[1],a_answer[2])
+else:
+    print(-1,-1,-1)
